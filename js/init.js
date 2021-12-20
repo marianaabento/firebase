350c190c82b7f16d13ae1b0e019db51aaa9ce4a6
marianaabento <95428399+marianaabento@users.noreply.github.com>
2021-12-20 12:57:23 -0300

    Add files via upload
    final

---
+++ js/init.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBFs5GYTlgftHErjkQVjPWE18RtxA_8R9M",
+    authDomain: "todo-list-57308.firebaseapp.com",
+    databaseURL: "https://todo-list-57308.firebaseapp.com", 
+    projectId: "todo-list-57308",
+    storageBucket: "todo-list-57308.appspot.com",
+    messagingSenderId: "624513207086",
+    appId: "1:624513207086:web:0ad599b46474232aaf48bf"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
